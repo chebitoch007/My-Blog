d5e42f077304c6a6d419bac62b9a2c5ae00438f8
chebitoch007 <eliphazchebitoch@gmail.com>
2025-11-27 16:23:28 +0300

    Fix S3 permissions and build script

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'chebitoch',
     'django_ckeditor_5',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -111,6 +112,57 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+# AWS S3 SETTINGS
+# We use env.str() to get keys. If they exist, we use S3.
+if env("AWS_ACCESS_KEY_ID", default=None):
+    AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
+    AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
+    AWS_S3_REGION_NAME = "eu-north-1"
+    AWS_S3_SIGNATURE_VERSION = "s3v4"
+    AWS_S3_OBJECT_PARAMETERS = {
+        "CacheControl": "max-age=86400",
+    }
+
+    # This makes sure files overwrite enabled so you don't get messy filenames
+    AWS_S3_FILE_OVERWRITE = False
+
+    # "public-read" makes sure your blog images are visible to visitors (Changed to None after error)
+    AWS_DEFAULT_ACL = None # Allow S3 Bucket Policy to handle permissions
+
+    # This configures Django to use S3 for storage
+    STORAGES = {
+        "default": {
+            "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+            "OPTIONS": {
+                "location": "media",  # Stores media in a 'media' folder on S3
+            },
+        },
+        "staticfiles": {
+            "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+            "OPTIONS": {
+                "location": "static",  # Stores static files in a 'static' folder on S3
+            },
+        },
+    }
+
+    # Point CKEditor 5 to the default storage (which is now S3)
+    CKEDITOR_5_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+    # Override URLs to point to S3
+    STATIC_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/static/"
+    MEDIA_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/media/"
+
+else:
+    # LOCAL DEVELOPMENT SETTINGS (Keep these as they were)
+    STATIC_URL = '/static/'
+    STATIC_ROOT = BASE_DIR / 'staticfiles'
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = BASE_DIR / 'media'
+    # Use local file storage for CKEditor locally
+    CKEDITOR_5_FILE_STORAGE = "django_ckeditor_5.storage.FileSystemStorage"
+
+"""
 #STATIC_URL = 'static/'
 # ================== Static & Media Files ==================
 STATICFILES_DIRS = [
@@ -119,10 +171,10 @@ STATICFILES_DIRS = [
 ]
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-"""STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'"""
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
-
+"""
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
@@ -130,8 +182,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 ADMIN_URL = env("ADMIN_URL", default="admin")
 
-CKEDITOR_5_FILE_STORAGE = "django_ckeditor_5.storage.FileSystemStorage"
 
+# ================== GLOBAL STATIC FILES CONFIG ==================
+# This tells Django where to look for your CSS/JS files locally
+STATICFILES_DIRS = [
+    BASE_DIR / 'chebitoch/static',
+    BASE_DIR / 'static',
+]
+
+# ================== CKEDITOR CONFIGURATION ==================
 CKEDITOR_5_CONFIGS = {
     "default": {
         "toolbar": [
